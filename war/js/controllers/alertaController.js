434867e43f265e0952e1573bf4308c56bc58949d
Risfaza <mrfemers@mail.com>
2019-06-14 11:55:10 -0500

    Alerta

--- war/js/controllers/alertaController.js
+++ war/js/controllers/alertaController.js
@@ -15,10 +15,28 @@ app.service('alertaService',['$http',
 			return d.promise;
 	}
 	
+	this.leer=function(id){
+		var d = $q.defer();
+		$http.get("alertas/resAlerta/"+id).then(
+			function(response) {
+				console.log(response);
+				d.resolve(response.data);
+			});
+		return d.promise;
+}
 }]);
 
 app.controller('alertaController',['alertaService','$scope',function(alertaService,$scope){
+	$scope.load = function(){
 	alertaService.consultar().then(function(data){
 		$scope.alertas=data;
 	})
+	}
+	$scope.load();
+	$scope.readAlert = function(id){
+		alertaService.leer(id).then(function(data){
+			alert(data);
+			$scope.load();
+		})
+	}
 }])
